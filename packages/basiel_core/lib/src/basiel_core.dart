41ac7e2513c599d604fd60bbd5a6b9f2fe69196a
Pieter-Jan Robrecht <robrechtpieterjan@gmail.com>
2025-02-15 12:18:01 +0100

    Add supabase dart packages

---
+++ packages/basiel_core/lib/src/basiel_core.dart
@@ -0,0 +1,7 @@
+/// {@template basiel_core}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class BasielCore {
+  /// {@macro basiel_core}
+  const BasielCore();
+}
---
+++ packages/basiel_core/test/src/basiel_core_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:basiel_core/basiel_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('BasielCore', () {
+    test('can be instantiated', () {
+      expect(BasielCore(), isNotNull);
+    });
+  });
+}
